3ee68811d0480a667a21bb92e145a34eb7295fbe
vNullptr <ghstdu@gmail.com>
2026-06-29 20:24:25 +0100

    setup

---
+++ rag/config.py
@@ -0,0 +1,8 @@
+"""pydantic .env setup"""
+
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    
+    model_config = SettingsConfigDict(env_file=".env", env_prefix="config_")
---
+++ rag/query/pipeline.py
@@ -0,0 +1,19 @@
+
+
+class QueryPipeline():
+    """
+    Defines the Query pipeline from embedding the query to retrieving top-k values/documents and citations to answer.
+    """
+    def __init__(self):
+        pass
+    
+    def answer(self)-> tuple[str ,list[str]]:
+        """Answers a question based on documents
+        
+        Returns:
+            A tuple (string answer, citations) representing what the llm answered based on retrieved documents.
+        """
+        citations: list[str] = []
+        answer: str = ""
+        
+        return (answer, citations)
