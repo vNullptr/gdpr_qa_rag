70a59667e80b8919a2481dc8a04ec694aaf3b26d
vNullptr <ghstdu@gmail.com>
2026-07-01 14:48:28 +0100

    add: Ingestion pipeline class

---
+++ rag/ingestion/pipeline.py
@@ -0,0 +1,16 @@
+
+
+class IngestionPipeline:
+    """
+    Defines the Ingestion pipeline from embedding documents and storing in the vector database.
+    """
+    def __init__(self):
+        pass
+    
+    def ingest(self):
+        """Ingests documents and stores them in vector database."""
+        pass
+        
+        
+    
+        
