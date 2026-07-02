7551afabb6bac80fc143e479c9df296c7a03a00b
vNullptr <ghstdu@gmail.com>
2026-07-02 19:38:37 +0100

    fix: IngestionPipeline class missing argument

--- tests/test_ingestion.py
+++ tests/test_ingestion.py
@@ -1,6 +1,6 @@
 from rag.ingestion.pipeline import IngestionPipeline
 
-ig_pipeline = IngestionPipeline()
+ig_pipeline = IngestionPipeline("./documents/eu_gdpr.html")
 
 def test_ingestion_return():
     assert ig_pipeline.ingest() == "Documents will be ingested !"
