0e29f7b4110fcf2c861dc5d3156246470018490a
vNullptr <ghstdu@gmail.com>
2026-07-05 19:34:24 +0100

    fix/add: Added loading of langsmith variable for tracing.

--- rag/config.py
+++ rag/config.py
@@ -1,7 +1,7 @@
 """pydantic .env setup"""
 
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
 
 class Settings(BaseSettings):
 
@@ -9,9 +9,27 @@ class Settings(BaseSettings):
     OPENAI_API_KEY: str
 
     # LANGCHAIN
+    LANGSMITH_TRACING: str
+    LANGSMITH_ENDPOINT: str
+    LANGSMITH_API_KEY: str
+    LANGSMITH_PROJECT: str
     
     # CONFIG
     MODEL_NAME: str
+    EMBEDDING_MODEL: str
+    
+    CHUNK_SIZE: int
+    CHUNK_OVERLAP: int
+    
+    # CHROMA
+    CHROMA_COLLECTION_NAME: str
     
     
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+    
+    def model_post_init(self, __context):
+        if self.LANGSMITH_TRACING and self.LANGSMITH_API_KEY:
+            os.environ["LANGSMITH_ENDPOINT"]= self.LANGSMITH_ENDPOINT
+            os.environ["LANGSMITH_TRACING"] = self.LANGSMITH_TRACING
+            os.environ["LANGSMITH_API_KEY"] = self.LANGSMITH_API_KEY
+            os.environ["LANGSMITH_PROJECT"] = self.LANGSMITH_PROJECT
