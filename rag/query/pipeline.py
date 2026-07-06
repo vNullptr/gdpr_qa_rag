bb6dd620b536b9d1a4951168f2b233b60cd247a9
vNullptr <ghstdu@gmail.com>
2026-07-06 18:08:46 +0100

    add: Testing retrieval

--- rag/query/pipeline.py
+++ rag/query/pipeline.py
@@ -1,4 +1,6 @@
 from langsmith import traceable 
+from langchain_core.runnables import RunnablePassthrough
+from rag.shared.vector_database import VectorDatabase
 
 class QueryPipeline:
     """
@@ -17,4 +19,15 @@ class QueryPipeline:
         citations: list[str] = []
         answer: str = ""
         
+        vector_db = VectorDatabase()
+        retriever = vector_db.get_retriever(method="similarity", top_k=2)
+        
+        
         return {"answer":answer, "citations":citations}
+    
+    
+    
+if __name__ == "__main__":
+    query_ppl = QueryPipeline()
+   
+    query_ppl.query("What is the right to erasure?") 
