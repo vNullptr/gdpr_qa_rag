a32ac3b8db38a3d609d0f3fb992d6c98863b53be
vNullptr <ghstdu@gmail.com>
2026-07-08 21:24:43 +0100

    add: evaless full query chain.

--- rag/query/pipeline.py
+++ rag/query/pipeline.py
@@ -1,33 +1,43 @@
-from langsmith import traceable 
+from langsmith import traceable
+from langsmith.wrappers import wrap_openai
 from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
 from rag.shared.vector_database import VectorDatabase
+from langchain_openai import ChatOpenAI
+from rag.query import prompts
+from rag.config import Settings
+
 
 class QueryPipeline:
     """
     Defines the Query pipeline from embedding the query to retrieving top-k values/documents and citations to answer.
     """
     def __init__(self):
-        pass
+        self.settings = Settings()
+        self.llm = ChatOpenAI(model=self.settings.MODEL_NAME, api_key=self.settings.OPENAI_API_KEY)
     
     @traceable(name="Pipeline Answer")
-    def query(self, query)-> dict:
+    def query(self, query: str)-> dict:
         """Answers a question based on documents
         
         Returns:
             A tuple (string answer, citations) representing what the llm answered based on retrieved documents.
         """
-        citations: list[str] = []
-        answer: str = ""
         
         vector_db = VectorDatabase()
-        retriever = vector_db.get_retriever(method="similarity", top_k=2)
+        retriever = vector_db.get_retriever(method=self.settings.SEARCH_METHOD, top_k=self.settings.TOP_K)
         
+        # I need to keep the retrieved documents so i can use them for references
+        chain = ({"documents":retriever, "question": RunnablePassthrough()}
+        | RunnablePassthrough.assign(
+            answer=(prompts.BASE_PROMPT 
+                    | self.llm 
+                    | StrOutputParser()
+                    )
+        ))
         
-        return {"answer":answer, "citations":citations}
+        result = chain.invoke(query)
+        references = [doc.metadata for doc in result["documents"]]
+        
+        return {"answer":result["answer"], "citations":references}
     
-    
-    
-if __name__ == "__main__":
-    query_ppl = QueryPipeline()
-   
-    query_ppl.query("What is the right to erasure?") 
