8da6f9bab643c4b6eb3f418262047f94c6931c15
vNullptr <ghstdu@gmail.com>
2026-07-08 22:27:37 +0100

    add/refactor: Updated ingestion tests (not finished).

--- tests/test_ingestion.py
+++ tests/test_ingestion.py
@@ -1,6 +1,28 @@
-from rag.ingestion.pipeline import IngestionPipeline
+import pytest
+from rag.ingestion.pipeline import IngestionPipeline, GDPRHTMLParser
 
-ig_pipeline = IngestionPipeline("./documents/eu_gdpr.html")
+#ig_pipeline = IngestionPipeline("./documents/eu_gdpr.html")
 
-def test_ingestion_return():
-    assert ig_pipeline.ingest() == "Documents will be ingested !"
+def test_parser_classifier():
+    assert GDPRHTMLParser.classify("Article 16") == ("article", "16")
+    assert GDPRHTMLParser.classify("CHAPTER III") == ("chapter", "III")
+    assert GDPRHTMLParser.classify("Section 3") == ("section", "3")
+    assert GDPRHTMLParser.classify("Random string") == ("text", None)
+    
+@pytest.fixture
+def articles():
+    with open("tests/fixtures/sample.html","r") as f:
+        parser = GDPRHTMLParser(f.read())
+        result = parser.parse()
+        
+        return result.articles
+    
+def test_parser_count(articles): 
+    assert len(articles) == 4
+    
+def test_parser_metadata(articles):
+    assert articles[0]["metadata"] == {"chapter": "I","chapter_title": "General provisions","section": 1,"article_title": "Subject-matter and objectives","article": 1}
+    
+def test_parser_new_chapter(articles):    
+    ch2_article = next(a for a in articles if a["metadata"]["chapter"] == "II")
+    assert ch2_article["metadata"]["section"] is None
